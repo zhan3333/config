66d4af9a23ab49512da1af3f43f0b9b8ce371384
zhan <grianchan@gmail.com>
2017-05-31 18:01:39 +0800

    增加测试用例

--- tests/ConfigTest.php
+++ tests/ConfigTest.php
@@ -9,6 +9,9 @@ use LoadConfig\Config;
 
 class ConfigTest extends PHPUnit_Framework_TestCase
 {
+    /**
+     * 测试加载文件夹
+     */
     public function testLoadDirPass()
     {
         $path = __DIR__.'/mocks/pass';
@@ -16,6 +19,9 @@ class ConfigTest extends PHPUnit_Framework_TestCase
         $this->assertEquals($config->get('config1.d.d1'), 'd');
     }
 
+    /**
+     * 测试加载文件
+     */
     public function testLoadFilePass()
     {
         $path = __DIR__.'/mocks/pass/config1.php';
@@ -24,11 +30,28 @@ class ConfigTest extends PHPUnit_Framework_TestCase
     }
 
     /**
+     * 测试加载空文件夹
      * @expectedException \LoadConfig\ConfigException
      */
     public function testLoadEmptyDir()
     {
         $path = __DIR__.'/mocks/empty';
         new Config($path);
+    }
+
+    /**
+     * @expectedException \LoadConfig\ConfigException
+     */
+    public function testLoadNoExistDir()
+    {
+        $path = __DIR__ . '/mocks/noExistDir';
+        new Config($path);
+    }
+
+    public function testLoadStrFile()
+    {
+        $path = __DIR__ . '/mocks/pass/returnStr.php';
+        $config = new Config($path);
+        $this->assertEquals($config->get('returnStr'), 'Abc');
     }
 }
---
+++ tests/mocks/pass/returnStr.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * @desc
+ * @author zhan <grianchan@gmail.com>
+ * @since 2017/5/31 17:59
+ */
+return 'Abc';
